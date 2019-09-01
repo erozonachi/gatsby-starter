222dd15a60f5ea3ff6bf92436e5800e80706da04
erozonachi <erozonachi.james@gmail.com>
2019-09-01 23:22:48 +0100

    Wrap `hello gatsby!` text in a h1 tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 import React from "react"
 
-export default () => <div style={{ color: `purple` }}>Hello Gatsby!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+  </div>
+)
